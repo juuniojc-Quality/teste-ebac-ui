0f7d9d340a2add4b4b3bf419cef437a92fbda666
Dário Júnior <juniorodrigueslol578@gmail.com>
2025-11-02 19:12:36 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,17 +13,32 @@ describe('Funcionalidade: Produtos', () => {
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Argus All-Weather Tank'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain' , produto )
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Apollo Running Short')
+        cy.get('.product_title').should('contain' , 'Apollo Running Short' )
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
+        let qtd = 7
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        produtosPage.addProdutoCarrinho('M' , 'Green' , qtd)
+        cy.get('.woocommerce-message').should('contain' , qtd + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[2].tamanho, 
+                dados[2].cor, 
+                dados[2].quantidade)
+            cy.get('.woocommerce-message').should('contain' , dados[2].nomeProduto)
+        })
     });
 });
